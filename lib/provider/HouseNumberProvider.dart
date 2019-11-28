beaf61c90f593d124166bbc7e1d019fc210c5cee
Justin <justin.vietz@gmx.de>
2019-11-28 03:05:01 +0100

    Created HouseNumberProvider
    Using Provider for statemanagement

---
+++ lib/provider/HouseNumberProvider.dart
@@ -0,0 +1,16 @@
+import 'package:brashapp/models/ApiResponse.dart';
+import 'package:brashapp/models/HouseNumberPickerModel.dart';
+import 'package:brashapp/provider/AbstractSpiderProvider.dart';
+
+class HouseNumberProvider extends AbstractSpiderProvider {
+
+  @override
+  ApiResponse parseJson(Map<String, dynamic> json) {
+    return HouseNumberPickerModel.fromJson(json);
+  }
+
+  @override
+  // TODO: implement spiderName
+  String get spiderName => "houseNumberPicker";
+
+}
