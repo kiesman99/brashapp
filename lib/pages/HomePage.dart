1f313bd93c0fff8c4f52a88605b8a5c051b97255
Justin <justin.vietz@gmx.de>
2019-11-28 02:22:23 +0100

    Moved CustomSearchDelegate
    Moved into own widget

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,12 +1,8 @@
-import 'package:brashapp/models/ApiResponse.dart';
-import 'package:brashapp/models/ErrorModel.dart';
-import 'package:brashapp/models/StreetPickerModel.dart';
-import 'package:brashapp/service/StreetPickerSpiderService.dart';
+import 'package:brashapp/widgets/CustomSearchDelegate.dart';
 import 'package:flutter/material.dart';
 import 'package:brashapp/widgets/OverviewWidget.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'dart:math';
 
 import 'HouseNumberPicker.dart';
 
@@ -107,105 +103,6 @@ class _HomePageState extends State<HomePage> {
       itemBuilder: (context, page){
         return OverviewWidget(searchQuery: items.elementAt(page));
       },
-    );
-  }
-}
-
-class CustomSearchDelegate extends SearchDelegate {
-
-  CustomSearchDelegate() : super(
-    searchFieldLabel: "Straße suchen",
-    textInputAction: TextInputAction.search
-  );
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    Future<ApiResponse> streets = StreetPickerSpiderService().getResponse(query);
-
-    return FutureBuilder<ApiResponse>(
-      future: streets,
-      builder: (context, snapshot){
-        if(snapshot.hasError){
-          return ListView(
-            children: <Widget>[
-              ListTile(
-                title: Text("Leider gab es einen Fehler."),
-              )
-            ],
-          );
-        }
-
-        if(snapshot.hasData){
-          if(snapshot.data is ErrorModel){
-            return ListView(
-              children: <Widget>[
-                ListTile(
-                  title: Text("Leider gab es einen Fehler."),
-                )
-              ],
-            );
-          }
-
-          StreetPickerModel _model = snapshot.data as StreetPickerModel;
-          return ListView.builder(
-            itemCount: _model.streets.length,
-            itemBuilder: (context, index){
-              return ListTile(
-                title: Text(_model.streets.elementAt(index).name),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => HouseNumberPicker(_model.streets.elementAt(index).href)
-                      )
-                  );
-                },
-              );
-            },
-          );
-        }
-
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-    );
-    return Center(
-      child: Text("Vielleicht was gefunden"),
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-
-    return ListView(
-      children: <Widget>[
-        ListTile(
-          title: Text("Suche nach einer Straße"),
-        )
-      ],
     );
   }
 }
