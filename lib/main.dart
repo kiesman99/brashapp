8911921e5ed4337d05c3fb424d81c5303db9fa96
Justin <justin.vietz@gmx.de>
2019-11-28 03:06:22 +0100

    Wrapped main with multiprovider
    This will give children access to the created provider instances

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
+import 'package:brashapp/provider/HouseNumberProvider.dart';
 import 'package:brashapp/utils/vm.dart' if (dart.library.html) 'package:brashapp/utils/js.dart';
 import 'package:brashapp/pages/HomePage.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
 
 
 void main() {
@@ -27,43 +29,48 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(builder: (_) => HouseNumberProvider()),
+      ],
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blue,
+          ),
+          home: FutureBuilder(
+              future: openBoxes(),
+              builder: (_, snapshot){
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.error != null) {
+                    print(snapshot.error);
+                    return Scaffold(
+                      body: Center(
+                        child: Text('Something went wrong :/'),
+                      ),
+                    );
+                  } else {
+                    return HomePage();
+                  }
+                } else {
+                  return Scaffold(
+                    body: Center(
+                      child: Text('Opening Hive...'),
+                    ),
+                  );
+                }
+              }
+          )
       ),
-      home: FutureBuilder(
-        future: openBoxes(),
-        builder: (_, snapshot){
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.error != null) {
-              print(snapshot.error);
-              return Scaffold(
-                body: Center(
-                  child: Text('Something went wrong :/'),
-                ),
-              );
-            } else {
-              return HomePage();
-            }
-          } else {
-            return Scaffold(
-              body: Center(
-                child: Text('Opening Hive...'),
-              ),
-            );
-          }
-        }
-      )
     );
   }
 }
