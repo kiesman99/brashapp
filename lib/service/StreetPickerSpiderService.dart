25f10dadce473f5df65a4d1c182b727532b44647
Justin <justin.vietz@gmx.de>
2019-11-28 02:24:16 +0100

    Changed queryformatter for Streetpicker
    Now using ISO-8859-1 to encode queryParameter

--- lib/service/StreetPickerSpiderService.dart
+++ lib/service/StreetPickerSpiderService.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:brashapp/models/ApiResponse.dart';
 import 'package:brashapp/models/StreetPickerModel.dart';
 import 'package:brashapp/service/AbstractSpiderService.dart';
@@ -11,8 +13,7 @@ class StreetPickerSpiderService extends AbstractSpiderService<StreetPickerModel>
    * The other URLs will be provided by the 'href' item in the response
    * JSON
    */
-  @override
-  String get urlPrefix => "http%3A%2F%2F213.168.213.236%2Fbremereb%2Fbify%2Fstrasse.jsp%3Fstrasse%3D";
+  String urlPrefix = "http://213.168.213.236/bremereb/bify/strasse.jsp?strasse=";
 
 
   @override
@@ -21,29 +22,12 @@ class StreetPickerSpiderService extends AbstractSpiderService<StreetPickerModel>
   }
   
   String _queryFormatter(String query) {
-    query = query.replaceAll(":", "%3A");
-    query = query.replaceAll("/", "%2F");
-    query = query.replaceAll("?", "%3F");
-    query = query.replaceAll("=", '%3D');
-    query = query.replaceAll("&", '%26');
-    query = query.replaceAll('%DF', "%25DF");
-    query = query.replaceAll('ß', "%25DF");
-    query = query.replaceAll("ä", "%25E4");
-    query = query.replaceAll("Ä", "%25C4");
-    query = query.replaceAll("ö", "%25F6");
-    query = query.replaceAll("Ö", "%25D6");
-    query = query.replaceAll("ü", "%25FC");
-    query = query.replaceAll("Ü", "%25DC");
-    query = query.replaceAll(" ", "%7F");
-
-    print("QUERY: $query");
-
-    return query;
+    return Uri.encodeQueryComponent(Uri.encodeQueryComponent(query, encoding: Encoding.getByName("iso_8859-1")), encoding: Encoding.getByName("iso_8859-1"));
   }
 
   @override
   Future<ApiResponse> getResponse(String searchQuery) {
-    return super.getResponse(_queryFormatter(searchQuery));
+    return super.getResponse(urlPrefix + _queryFormatter(searchQuery));
   }
 
 
