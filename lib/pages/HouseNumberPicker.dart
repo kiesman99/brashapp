a701848dc09ce9e9725fd6ffca0ae83d5d9ebcf4
Justin <justin.vietz@gmx.de>
2019-11-28 03:05:31 +0100

    Added Provider to HouseNumberPicker

--- lib/pages/HouseNumberPicker.dart
+++ lib/pages/HouseNumberPicker.dart
@@ -2,11 +2,13 @@ import 'package:brashapp/models/ApiResponse.dart';
 import 'package:brashapp/models/ErrorModel.dart';
 import 'package:brashapp/models/HouseNumberPickerModel.dart';
 import 'package:brashapp/pages/FinalSelectionPage.dart';
+import 'package:brashapp/provider/HouseNumberProvider.dart';
 import 'package:brashapp/service/HouseNumberPickerSpiderService.dart';
 import 'package:brashapp/widgets/ApiHandlerWidget.dart';
 import 'package:flutter/material.dart';
 
 import 'package:brashapp/widgets/OverviewWidget.dart';
+import 'package:provider/provider.dart';
 
 class HouseNumberPicker extends StatefulWidget {
 
@@ -25,11 +27,51 @@ class _HouseNumberPickerState extends State<HouseNumberPicker> {
   @override
   void initState() {
     super.initState();
-    houseNumbers = HouseNumberPickerSpiderService().getResponse(widget.searchQuery);
   }
 
   @override
   Widget build(BuildContext context) {
+    Provider.of<HouseNumberProvider>(context).fetch(widget.searchQuery);
+    return Consumer<HouseNumberProvider>(
+      builder: (context, provider, _){
+        if(provider.response is ErrorModel){
+          ErrorModel model = provider.response as ErrorModel;
+          return Scaffold(
+            body: Center(
+              child: Text(model.message),
+            ),
+          );
+        }else if(provider.response is HouseNumberPickerModel){
+          HouseNumberPickerModel model = provider.response as HouseNumberPickerModel;
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(model.street),
+            ),
+            body: ListView.builder(
+              itemCount: model.houseNumbers.length,
+              itemBuilder: (_, index){
+                return ListTile(
+                    onTap: (){
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => FinalSelectionPage(searchQuery: model.houseNumbers[index].href),
+                          ));
+                    },
+                    title: Text(model.houseNumbers[index].number.toString())
+                );
+              },
+            ),
+          );
+        }
+
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
+
     return ApiHandlerWidget(
       future: houseNumbers,
       childBuilder: (response){
