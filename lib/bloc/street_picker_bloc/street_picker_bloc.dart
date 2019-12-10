9712961aaf8bfd092a3aa9c02edee96a716d5eaa
Justin <justin.vietz@gmx.de>
2019-12-10 22:45:51 +0100

    Added basic functionality of StreetPickerBloc

---
+++ lib/bloc/street_picker_bloc/street_picker_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:bloc/bloc.dart';
+import 'package:brashapp/api/repositories/BrashRepository.dart';
+import 'package:brashapp/bloc/street_picker_bloc/street_picker_event.dart';
+import 'package:brashapp/bloc/street_picker_bloc/street_picker_state.dart';
+import 'package:brashapp/models/StreetPickerModel.dart';
+import 'package:meta/meta.dart';
+
+class StreetPickerBloc extends Bloc<StreetPickerEvent, StreetPickerState> {
+
+  StreetPickerBloc(this.brashRepository) : 
+    assert(brashRepository != null);
+
+  BrashRepository brashRepository;
+
+  @override
+  StreetPickerState get initialState => StreetPickerInitial();
+
+  @override
+  Stream<StreetPickerState> mapEventToState(StreetPickerEvent event) async* {
+    if(event is StreetPickerFetch) {
+      yield StreetPickerFetchInProgress();
+      try {
+        final Streets streets = await brashRepository.getStreets(event.searchQuery);
+        yield StreetPickerFetchSuccess(streets: streets);
+      } catch(e) {
+        yield StreetPickerFetchFailure(errorMessage: e.toString());
+      }
+    }
+  }
+  
+}
