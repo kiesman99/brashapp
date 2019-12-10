4412570e0189f0624283ca685b2bcc5430051ff6
Justin <justin.vietz@gmx.de>
2019-12-10 15:39:45 +0100

    corrected suggestions from linter

--- test/brash_repository_test.dart
+++ test/brash_repository_test.dart
@@ -6,26 +6,26 @@ import 'package:brashapp/models/StreetPickerModel.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 
-main() {
+void main() {
 
-  group("BrashRepositoryTests", () {
+  group('BrashRepositoryTests', () {
 
     BrashRepository repository;
 
     setUp(() {
-      repository = new BrashRepository(
-        apiClient: new BrashApiClient(
+      repository = BrashRepository(
+        apiClient: BrashApiClient(
           httpClient: http.Client()
         )
       );
     });
 
-    group("AddressInformation", () {
+    group('AddressInformation', () {
       test('Fetch existing AddressInformation', () async {
-        final String url = "http://213.168.213.236/bremereb/bify/bify.jsp?strasse=Emanuelstra%25DFe%26hausnummer=15";
-        AddressInformation response = await repository.getAddressInformation(url);
+        const String url = 'http://213.168.213.236/bremereb/bify/bify.jsp?strasse=Emanuelstra%25DFe%26hausnummer=15';
+        final AddressInformation response = await repository.getAddressInformation(url);
         print(response.currentStreet);
-        expect(response.currentStreet.length > 0, true);
+        expect(response.currentStreet.isNotEmpty, true);
         expect(response.allTrashEntries.isNotEmpty, true);
         expect(response.pickUpDay.isNotEmpty, true);
 
@@ -38,10 +38,10 @@ main() {
       });
 
       test('Fetch existing AddressInformation (street with umlaut)', () async {
-        final String url = "http://213.168.213.236/bremereb/bify/bify.jsp?strasse=%25D6tzener%2520Weg%26hausnummer=2";
-        AddressInformation response = await repository.getAddressInformation(url);
+        const String url = 'http://213.168.213.236/bremereb/bify/bify.jsp?strasse=%25D6tzener%2520Weg%26hausnummer=2';
+        final AddressInformation response = await repository.getAddressInformation(url);
         print(response.currentStreet);
-        expect(response.currentStreet.length > 0, true);
+        expect(response.currentStreet.isNotEmpty, true);
         expect(response.allTrashEntries.isNotEmpty, true);
         expect(response.pickUpDay.isNotEmpty, true);
 
@@ -54,16 +54,16 @@ main() {
       });
 
       test('Fetch non-existing AddressInformation', () async {
-        final String url = "http://213.168.213.236/bremereb/bify/bify.jsp?strasse=Emanuelstra%25DFe%26hausnummer=32";
+        const String url = 'http://213.168.213.236/bremereb/bify/bify.jsp?strasse=Emanuelstra%25DFe%26hausnummer=32';
         expect(() async => await repository.getAddressInformation(url), throwsException);
       });
     });
 
-    group("StreetPicker", () {
+    group('StreetPicker', () {
 
       test("Fetch all Streets starting with 'A'", () async {
-        final String query = "A";
-        Streets response = await repository.getStreets(query);
+        const String query = 'A';
+        final Streets response = await repository.getStreets(query);
         expect(response.streets.isNotEmpty, true);
         // test first element
         expect(response.streets[0].name.isNotEmpty, true);
@@ -74,8 +74,8 @@ main() {
       });
 
       test("Fetch all Streets starting with 'Ü' (umlaut)", () async {
-        final String query = "Ü";
-        Streets response = await repository.getStreets(query);
+        const String query = 'Ü';
+        final Streets response = await repository.getStreets(query);
         expect(response.streets.isNotEmpty, true);
         // test first element
         expect(response.streets[0].name.isNotEmpty, true);
@@ -86,8 +86,8 @@ main() {
       });
 
       test("Fetch all Streets starting with 'Emanu' (more than one letter)", () async {
-        final String query = "Ü";
-        Streets response = await repository.getStreets(query);
+        const String query = 'Ü';
+        final Streets response = await repository.getStreets(query);
         expect(response.streets.isNotEmpty, true);
         // test first element
         expect(response.streets[0].name.isNotEmpty, true);
@@ -98,22 +98,22 @@ main() {
       });
 
       test("Fetch for non existing street 'Ä'", () async {
-        final String query = "Ä";
+        const String query = 'Ä';
         expect(() async => await repository.getStreets(query), throwsException);
       });
 
       test("Fetch for non existing street 'Blubedi'", () async {
-        final String query = "Blubedi";
+        const String query = 'Blubedi';
         expect(() async => await repository.getStreets(query), throwsException);
       });
 
     });
 
-    group("HouseNumberPicker", () {
+    group('HouseNumberPicker', () {
 
       test("Fetch Housenumbers for street 'Emanuelstraße'", () async {
-        final String url = "http://213.168.213.236/bremereb/bify/hausnummer.jsp?strasse=Emanuelstra%2525DFe";
-        HouseNumbers response = await repository.getHouseNumbers(url);
+        const String url = 'http://213.168.213.236/bremereb/bify/hausnummer.jsp?strasse=Emanuelstra%2525DFe';
+        final HouseNumbers response = await repository.getHouseNumbers(url);
         expect(response.houseNumbers.isNotEmpty, true);
         expect(response.street.isNotEmpty, true);
         // test first element in list
@@ -125,9 +125,9 @@ main() {
       });
 
       test("Fetch HouseNumbers for a non-existing street 'Blubedi'", () async {
-        final String url = "http://213.168.213.236/bremereb/bify/hausnummer.jsp?strasse=Blubedi";
+        const String url = 'http://213.168.213.236/bremereb/bify/hausnummer.jsp?strasse=Blubedi';
         expect(() async => await repository.getHouseNumbers(url), throwsException);
-      }, skip: "Skipping because the api does return the error in the currentStreet attribute");
+      }, skip: 'Skipping because the api does return the error in the currentStreet attribute');
 
     });
 
