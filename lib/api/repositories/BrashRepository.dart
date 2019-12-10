9a881b4841f7ea92937ae7c6dc50588eec5ad3ed
Justin <justin.vietz@gmx.de>
2019-12-10 12:29:49 +0100

    Created BrashRepository
    The Repository is the interface that should be used to make calls to the brash api server

---
+++ lib/api/repositories/BrashRepository.dart
@@ -0,0 +1,33 @@
+import 'package:brashapp/api/client/BrashApiClient.dart';
+import 'package:brashapp/models/AddressInformation.dart';
+import 'package:brashapp/models/HouseNumberPickerModel.dart';
+import 'package:brashapp/models/StreetPickerModel.dart';
+import 'package:flutter/material.dart';
+
+class BrashRepository {
+
+  final BrashApiClient apiClient;
+
+  BrashRepository({
+    @required this.apiClient
+  }) : assert(apiClient != null);
+
+  /// This function will get all available streets 
+  /// of the provided [searchQuery]
+  Future<Streets> getStreets(String searchQuery) async {
+    return await apiClient.getStreets(searchQuery);
+  }
+
+  /// This function will get all avaiable house numbers of the
+  /// provided [streetUrl]
+  Future<HouseNumbers> getHouseNumbers(String streetUrl) async {
+    return await apiClient.getHouseNumbers(streetUrl);
+  }
+
+  /// This function will get the trash information of the provided 
+  /// addess via the [addressUrl]
+  Future<AddressInformation> getAddressInformation(String addressUrl) async {
+    return await apiClient.getAddressInformation(addressUrl);
+  }
+
+}
