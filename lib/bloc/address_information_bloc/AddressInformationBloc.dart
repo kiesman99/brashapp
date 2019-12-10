1886ab995d98714fa372c839e372ff6984e805fc
Justin <justin.vietz@gmx.de>
2019-12-10 15:08:34 +0100

    Added logic handling if events will come in

--- lib/bloc/address_information_bloc/AddressInformationBloc.dart
+++ lib/bloc/address_information_bloc/AddressInformationBloc.dart
@@ -1,19 +1,31 @@
 import 'package:bloc/bloc.dart';
+import 'package:brashapp/api/repositories/BrashRepository.dart';
 import 'package:brashapp/bloc/address_information_bloc/AddressInformationEvent.dart';
 import 'package:brashapp/bloc/address_information_bloc/AddressInformationState.dart';
+import 'package:brashapp/models/AddressInformation.dart';
 
 /// This Bloc will handle resolving the Information
 /// for a selected address
 
 class AddressInformationBloc extends Bloc<AddressInformationEvent, AddressInformationState> {
 
+  AddressInformationBloc(this.brashRepository);
+
+  final BrashRepository brashRepository;
+
   @override
   AddressInformationState get initialState => Loading();
 
   @override
-  Stream<AddressInformationState> mapEventToState(AddressInformationEvent event) {
+  Stream<AddressInformationState> mapEventToState(AddressInformationEvent event) async* {
     if(event is Fetch){
-      
+      yield Loading();
+      try {
+        final AddressInformation information = await brashRepository.getAddressInformation(event.url);
+        yield Loaded(addressInformation: information);
+      }catch(e) {
+        yield Error(errorMessage: e.toString());
+      }
     }
   }
   
